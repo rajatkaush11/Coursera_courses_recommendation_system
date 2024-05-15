b0aac69e73229c5a2a5524b76b1e374e836e28ce
rajatkaush11 <111580832+rajatkaush11@users.noreply.github.com>
2024-05-15 18:40:10 +0530

    Update app.py

--- Deployment_files/app.py
+++ Deployment_files/app.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Title and Description with Styling
-st.set_page_config(page_title=" <br>Coursera </br> Course Recommendation System", page_icon="📘")
-st.title("Coursera Course Recommendation System")
+st.set_page_config(page_title=" Coursera Course Recommendation System", page_icon="📘")
+st.title("<br>Coursera</br> Course Recommendation System")
 st.markdown(
     """
     <style>
